74e480a020f19f9ccb93c580059fc0b7f01d446b
Yakov Karpov <yakovkarpoff@gmail.com>
2022-02-13 03:26:32 +0300

    Fix app dependencies

--- lib/common/extension/src/build_context.dart
+++ lib/common/extension/src/build_context.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:purple_starter/common/database/app_database.dart';
-import 'package:purple_starter/common/widget/app_dependencies_provider.dart';
+import 'package:purple_starter/common/widget/app_dependencies_scope.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 extension BuildContextX on BuildContext {
