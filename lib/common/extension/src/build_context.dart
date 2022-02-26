6875c8e83fdb61009fd3cf3e0b056b87eb7a7441
Yakov Karpov <yakovkarpoff@gmail.com>
2022-02-26 04:14:38 +0300

    Add missing pacakges (#23)
    * Add missing pacakges
    
    * Fix comment

--- lib/common/extension/src/build_context.dart
+++ lib/common/extension/src/build_context.dart
@@ -15,6 +15,7 @@ extension BuildContextX on BuildContext {
   AppLocalizations get localized => AppLocalizations.of(this)!;
 
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+  Size get screenSize => mediaQuery.size;
 
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => theme.textTheme;
