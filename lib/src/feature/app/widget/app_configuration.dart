5d5e5f5355f1705d59f78767aac7ec4952d62487
Yakov Karpov <yakovkarpoff@gmail.com>
2022-04-23 00:19:32 +0300

    Misc fixes (#45)
    * Fix Scope listening
    
    * Fix AppBlocObserver logs writing

--- lib/src/feature/app/widget/app_configuration.dart
+++ lib/src/feature/app/widget/app_configuration.dart
@@ -12,17 +12,21 @@ class AppConfiguration extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => AppRouterBuilder(
-        createRouter: (context) => AppRouter(),
-        builder: (context, parser, delegate) => MaterialApp.router(
-          routeInformationParser: parser,
-          routerDelegate: delegate,
-          onGenerateTitle: (context) => context.localized.appTitle,
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          themeMode: SettingsScope.themeModeOf(context),
-        ),
-      );
+  Widget build(BuildContext context) {
+    final themeMode = SettingsScope.themeModeOf(context, listen: true);
+
+    return AppRouterBuilder(
+      createRouter: (context) => AppRouter(),
+      builder: (context, parser, delegate) => MaterialApp.router(
+        routeInformationParser: parser,
+        routerDelegate: delegate,
+        onGenerateTitle: (context) => context.localized.appTitle,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: themeMode,
+      ),
+    );
+  }
 }
