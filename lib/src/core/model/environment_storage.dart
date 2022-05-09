16fccafbd407b28a3d4221f0fd2815eb297ac794
Yakov Karpov <yakovkarpoff@gmail.com>
2022-05-09 23:38:17 +0300

    Add env environment value

--- lib/src/core/model/environment_storage.dart
+++ lib/src/core/model/environment_storage.dart
@@ -1,9 +1,17 @@
+import 'package:purple_starter/src/core/enum/environment.dart';
+
 abstract class IEnvironmentStorage {
+  Environment get environment;
   String get sentryDsn;
 }
 
 class EnvironmentStorage implements IEnvironmentStorage {
   const EnvironmentStorage();
+
+  @override
+  Environment get environment => Environment.values.byName(
+        const String.fromEnvironment('ENVIRONMENT'),
+      );
 
   @override
   String get sentryDsn => const String.fromEnvironment('SENTRY_DSN');
